b1cc66398cf8a18acae472fa65980bb78ef2ce78
EddieFranco <46496091+EddieFranco@users.noreply.github.com>
2019-04-08 10:52:22 -0600

    updated contac us

--- app.js
+++ app.js
@@ -11,3 +11,18 @@ function chocolateCakeRecipe(){
        });
    });
 }
+
+function initMap() {
+    var myLatLng = {lat: 40.233845, lng: -111.658531};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 14,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  }
